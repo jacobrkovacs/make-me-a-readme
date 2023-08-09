12f843ac637205301521fabc9bed77f0b2f380e6
Jacob Kovacs <jwkovacs.19@gmail.com>
2023-08-09 13:31:45 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+// TODO: Include packages needed for this application
+const fs = require('fs');
+
+// TODO: Create an array of questions for user input
+const questions = [
+    "What is the title of your Application?",
+    "What is a brief description of your application?",
+    "",
+    "",
+    "",
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFile()
+}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
